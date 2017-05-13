b53cc010f30c325f8759859c4ec6cdabc59cb4a0
Hannah Nordgren <hey@hereshannahs.info>
2017-05-13 16:53:48 -0400

    [WIP] Here we go yo

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+let nextTodo = 0;
+
+export const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodo++,
+    text
+  }
+}
+
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_ FILTER',
+    filter
+  }
+}
+
+export const toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
---
+++ src/components/app/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Footer from '../footer/footer';
+import AddTodo from '../../containers/add_todo/add_todo';
+import VisibleTodoList from '../../containers/visibleTodoList/visibleTodoList';
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
